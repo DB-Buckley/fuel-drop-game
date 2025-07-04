0902a323317bc1a6c558cf3e7bd830b7586daede
DB-Buckley <david.b.9307@gmail.com>
2025-07-04 21:39:50 +0200

    Update assets.js

--- assets.js
+++ assets.js
@@ -1,44 +1,46 @@
-const images = {
-  car: new Image(),
-  fuel_gold: new Image(),
-  fuel_bonus: new Image(),
-  fuel_green: new Image(),
-  banner_bonus: new Image(),
-  banner_increase: new Image(),
-  banner_decrease: new Image(),
-};
+(() => {
+  const images = {
+    car: new Image(),
+    fuel_gold: new Image(),
+    fuel_bonus: new Image(),
+    fuel_green: new Image(),
+    banner_bonus: new Image(),
+    banner_increase: new Image(),
+    banner_decrease: new Image(),
+  };
 
-const imagePaths = {
-  car: 'assets/car.png',
-  fuel_gold: 'assets/fuel_gold.png',
-  fuel_bonus: 'assets/fuel_bonus.png',
-  fuel_green: 'assets/fuel_green.png',
-  banner_bonus: 'assets/banner_bonus.png',
-  banner_increase: 'assets/banner_increase.png',
-  banner_decrease: 'assets/banner_decrease.png',
-};
+  const imagePaths = {
+    car: 'assets/car.png',
+    fuel_gold: 'assets/fuel_gold.png',
+    fuel_bonus: 'assets/fuel_bonus.png',
+    fuel_green: 'assets/fuel_green.png',
+    banner_bonus: 'assets/banner_bonus.png',
+    banner_increase: 'assets/banner_increase.png',
+    banner_decrease: 'assets/banner_decrease.png',
+  };
 
-function loadImages(callback) {
-  let imagesLoaded = 0;
-  const totalImages = Object.keys(imagePaths).length;
+  function loadImages(callback) {
+    let imagesLoaded = 0;
+    const totalImages = Object.keys(imagePaths).length;
 
-  for (const key in imagePaths) {
-    images[key].src = imagePaths[key];
-    images[key].onload = () => {
-      imagesLoaded++;
-      if (imagesLoaded === totalImages) {
-        callback?.();
-      }
-    };
-    images[key].onerror = () => {
-      console.warn(`Failed to load image: ${imagePaths[key]}`);
-      imagesLoaded++;
-      if (imagesLoaded === totalImages) {
-        callback?.();
-      }
-    };
+    for (const key in imagePaths) {
+      images[key].src = imagePaths[key];
+      images[key].onload = () => {
+        imagesLoaded++;
+        if (imagesLoaded === totalImages) {
+          callback?.();
+        }
+      };
+      images[key].onerror = () => {
+        console.warn(`Failed to load image: ${imagePaths[key]}`);
+        imagesLoaded++;
+        if (imagesLoaded === totalImages) {
+          callback?.();
+        }
+      };
+    }
   }
-}
 
-window.images = images;
-window.loadImages = loadImages;
+  window.images = images;
+  window.loadImages = loadImages;
+})();
