9eb3627aad609cdfdfeb763503816e8bbefde148
DB-Buckley <david.b.9307@gmail.com>
2025-07-05 12:35:07 +0200

    Update assets.js

--- assets.js
+++ assets.js
@@ -12,8 +12,8 @@
     mzansiLogo: 'assets/mzansi_logo.png',
     splash_desktop: 'assets/splash_desktop.png',
     splash_mobile: 'assets/splash_mobile.png',
-    bg_play_area_desktop: 'assets/bg_desktop.jpg',  // added as path string
-    bg_play_area_mobile: 'assets/bg_mobile.jpg',    // added as path string
+    bg_play_area_desktop: 'assets/bg_desktop.png',  // added as path string
+    bg_play_area_mobile: 'assets/bg_mobile.png',    // added as path string
   };
 
   const images = {};
